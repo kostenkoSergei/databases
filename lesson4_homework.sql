4de8e6f41bbcddf7693e6a792bd4813ee44a5db9
kostenkoSergei <sdh1985@mail.ru>
2020-07-29 21:12:15 +0300

    first commit

---
+++ lesson4_homework.sql
@@ -0,0 +1,16 @@
+USE vk;
+
+/* Задание 1. Заполнить все таблицы БД vk данными (по 10-100 записей в каждой таблице)*/
+INSERT INTO users VALUES
+	(NULL, 'Tom', 'Hanks', 'test1@example.com', NULL, 5555555),
+	(NULL, 'Ann', 'Swan', 'test2@example.com', NULL, 5555556),
+	(NULL, 'Jack', 'Vorobiev', 'test3@example.com', NULL, 5555557),
+	(NULL, 'Jim', 'Pupkin', 'test4@example.com', NULL, 5555558),
+	(NULL, 'Donald', 'Trump', 'test5@example.com', NULL, 5555559),
+	(NULL, 'Conor', 'McGregor', 'test6@example.com', NULL, 5555560),
+	(NULL, 'Dana', 'White', 'test7@example.com', NULL, 5555561),
+	(NULL, 'Jana', 'Petrova', 'test8@example.com', NULL, 5555562),
+	(NULL, 'Steven', 'Thompson', 'test9@example.com', NULL, 5555563),
+	(NULL, 'Kevin', 'Kattar', 'test10@example.com', NULL, 5555564),
+	(NULL, 'Vasya', 'Pupkin', 'test11@example.com', NULL, 5555565),
+	(NULL, 'Grishka', 'Otrepiev', 'test12@example.com', NULL, 5555568);

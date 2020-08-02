6da6e70e836c640aab2553f040dd24671f4ee7e4
kostenkoSergei <sdh1985@mail.ru>
2020-08-02 15:42:46 +0300

    added boolean type and generated column

--- doc_flow_db_select.sql
+++ doc_flow_db_select.sql
@@ -1,6 +1,6 @@
 USE doc_flow;
 
--- select all projects and corresponding customers
+-- 1. select all projects and corresponding customers
 SELECT 
 	p.name AS projects_list , c.name AS customers_list
 FROM 
@@ -8,12 +8,21 @@ FROM
 INNER JOIN customers c ON p.customer_id = c.id
 ORDER BY c.name;
 
--- select all working documentation corresponding with project 'Строительство ПС 330 кВ Мурманская'
+-- 2. select all working documentation corresponding with project 'Строительство ПС 330 кВ Мурманская'
 SELECT code, name, started_at, finished_at FROM documentation 
 WHERE 
 	project_id = (SELECT id FROM projects WHERE name LIKE '%Мурманская%')
 AND 
 	stage_id = (SELECT id FROM stages WHERE name = 'РД');
 
-
-
+-- 3. to analyze what documentation is more time consuming for development
+SET @some_project_id = 1;
+SELECT 
+	code, d.name, s.name, DATEDIFF(finished_at, started_at) AS `time_to_develop(days)`
+FROM 
+	documentation d
+INNER JOIN stages s ON s.id = d.stage_id
+WHERE 
+	d.project_id = @some_project_id
+ORDER BY 
+	`time_to_develop(days)` DESC;

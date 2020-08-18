45456b0a4071fcf67c526c95ce835167e67ddef9
kostenkoSergei <sdh1985@mail.ru>
2020-08-18 19:40:40 +0300

    added triggers to protest invoice table and added description

--- doc_flow_triggers.sql
+++ doc_flow_triggers.sql
@@ -1,6 +1,6 @@
 USE doc_flow;
 
-/* 1. trigger to automatically set `is_sent` value to 1 in `versions` table 
+/* 1. Trigger to automatically set `is_sent` value to 1 in `versions` table 
 when corresponding documentation is added to `dispatches` table
  */
 DROP TRIGGER IF EXISTS change_is_sent_status;
@@ -15,9 +15,10 @@ CREATE
 			INNER JOIN dispatches d ON d.documentation_id = v.id
 			SET v.is_sent = 1;
 		END $
+		
 DELIMITER ;
 
-/* to check: add versions of documentation in `version` table (before doing you have to execute
+/* To check: add versions of documentation in `version` table (before doing you have to execute
 procedures 3 and 4 to add new project (project_id = 16) and new documetation) with `is_sent` value = default (0)
  */
 INSERT INTO versions VALUES
@@ -25,18 +26,18 @@ INSERT INTO versions VALUES
 	(NULL, 153, 0, '2020-08-01', DEFAULT), (NULL, 154, 0, '2020-08-01', DEFAULT),
 	(NULL, 155, 0, '2020-08-01', DEFAULT), (NULL, 156, 0, '2020-07-20', DEFAULT);
 
--- check that `is_sent` in last four new added versions equal 0
+-- Check that `is_sent` in last four new added versions equal 0
 SELECT * FROM versions ORDER BY id DESC LIMIT 4;
 
--- now create new invoice to send our new added documentation to customer 
+-- Now create new invoice to send our new added documentation to customer 
 INSERT INTO invoices VALUES
 	(NULL, 'ПИР-175-И-2020-003', '2020-08-14', DEFAULT, 5, 21); -- 'ПС 220 кВ Озерная с заходами ВЛ 220 кВ' project_id = 16
 	
--- now create new dispatches to sent our 4 books of new added documentation
+-- Now create new dispatches to sent our 4 books of new added documentation
 INSERT INTO dispatches VALUES
 	(NULL, 29, 232), (NULL, 29, 233), (NULL, 29, 234), (NULL, 29, 235); -- project_id 16	
 
--- now check to be sure trigger `change_is_sent_status` did it's work
+-- Now check to be sure trigger `change_is_sent_status` did it's work
 SELECT * FROM versions ORDER BY id DESC LIMIT 4;
 -- `is_sent` automatically changed from 0 to 1 after adding new dicumentation in dispatches
 	
@@ -45,7 +46,7 @@ SELECT * FROM versions ORDER BY id DESC LIMIT 4;
  */
 DROP FUNCTION IF EXISTS convert_func;
 
--- function to convert russian letters into english 
+-- Function to convert russian letters into english 
 DELIMITER $$
 CREATE FUNCTION convert_func (original VARCHAR(512)) RETURNS VARCHAR(512) CHARSET utf8 DETERMINISTIC
 	BEGIN
@@ -103,7 +104,7 @@ CREATE FUNCTION convert_func (original VARCHAR(512)) RETURNS VARCHAR(512) CHARSE
  
 DELIMITER ;
 
--- trigger automatically sets an email for new added employee using convert_func
+-- Trigger automatically sets an email for new added employee using convert_func
 DROP TRIGGER IF EXISTS set_emp_email;
 
 DELIMITER $
@@ -118,8 +119,60 @@ CREATE
 			SET @rus_mail = LOWER(CONCAT(@lastname, '_', SUBSTRING(@firstname, 1, 1), SUBSTRING(@middlename, 1, 1)));
 			SET NEW.email = convert_func (@rus_mail);
 		END $
+		
 DELIMITER ;
 
--- to check. email was automatically established
+-- To check. Email was automatically established
 INSERT INTO employees (id,  lastname, firstname, middlename, department_id, division_id, phone_id, position_id) VALUES
 	(NULL, 'Андреев', 'Борис', 'Владимирович', 1, 1, 1, 5);
+
+/* 3. Trigger to validate a number of invoice before adding it in invoice table.
+number of invoice has to correspond with first seven digits of contract number
+ */
+DROP TRIGGER IF EXISTS check_invoice_insert;
+
+DELIMITER $
+CREATE 
+	TRIGGER check_invoice_insert
+	BEFORE INSERT 
+	ON invoices FOR EACH ROW 
+		BEGIN 
+			DECLARE warn_msg varchar(128);
+			IF (SELECT LEFT(NEW.invoice_number, 7)) NOT IN (SELECT contract_number FROM projects) THEN
+				SET warn_msg = CONCAT('TriggerError: Trying to insert not valid number for invoice: ', NEW.invoice_number);
+				SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = warn_msg;
+			END IF;
+		END $
+		
+DELIMITER ;
+	
+-- To check. Add new invoice with invoice number that doesn't correspond to any project contract number
+INSERT INTO invoices VALUES
+	(NULL, 'ПИР-274-И-2020-002', '2020-06-25', DEFAULT, 1, 21); -- ПИР-274 doesn't exists in projcts table
+
+/* Trigger to validate a number of invoice before updating it in invoice table.
+number of invoice has to correspond with first seven digits of contract number.
+This trigger has to protect from changing present invoice numbers in table
+ */
+
+DROP TRIGGER IF EXISTS check_invoice_update;
+
+DELIMITER $
+CREATE 
+	TRIGGER check_invoice_update
+	BEFORE UPDATE 
+	ON invoices FOR EACH ROW 
+		BEGIN 
+			DECLARE warn_msg varchar(128);
+			IF (SELECT LEFT(NEW.invoice_number, 7)) NOT IN (SELECT contract_number FROM projects) THEN
+				SET warn_msg = CONCAT('TriggerError: Trying to update to not valid number for invoice: ', NEW.invoice_number);
+				SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = warn_msg;
+			END IF;
+		END $
+		
+DELIMITER ;
+
+-- To check. Update existing invoice with invoice number that doesn't correspond to any project contract number
+UPDATE invoices 
+	SET invoice_number = 'ПИР-375-И-2018-002' WHERE id = 1; -- ПИР-375 doesn't exists in projcts table
+

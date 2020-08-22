9603f04c271a72ff2cc71ea22d2798156fff6519
kostenkoSergei <sdh1985@mail.ru>
2020-08-22 18:12:59 +0300

    finished procedures with commits

--- doc_flow_procedures.sql
+++ doc_flow_procedures.sql
@@ -2,9 +2,10 @@ USE doc_flow;
 /* 1. procedure to request some list of documentation knowing
  part of project name and stage
  */
-DROP PROCEDURE IF EXISTS sp_show_docs_doc_flow;
+DROP PROCEDURE IF EXISTS sp_show_docs;
+
 DELIMITER $
-CREATE PROCEDURE sp_show_docs_doc_flow (proj_name_part VARCHAR(50), stage_name VARCHAR(3))
+CREATE PROCEDURE sp_show_docs (proj_name_part VARCHAR(50), stage_name VARCHAR(3))
 BEGIN
 	SELECT 
 		p.name AS title, d.code AS code, d.name AS documentation, 
@@ -18,43 +19,47 @@ BEGIN
 	INNER JOIN employees e ON d.curator_id = e.id 
 	INNER JOIN phone_numbers ph ON e.phone_id = ph.id
 	WHERE p.name LIKE CONCAT('%', proj_name_part, '%') AND s.name = stage_name;
+
 END $
+
 DELIMITER ;
 
-CALL sp_show_docs_doc_flow ('Псков', 'РД');
-CALL sp_show_docs_doc_flow ('Харан', 'ПД');
+CALL sp_show_docs ('Псков', 'РД');
+CALL sp_show_docs ('Харан', 'ПД');
 
-/* 2. to add some customer, or subcontractor, or gencontractor
+/* 2. procedure to add some customer, or subcontractor, or gencontractor
 (this three tables have the same structure)
  */
-DROP PROCEDURE IF EXISTS sp_insert_csg_doc_flow;
+DROP PROCEDURE IF EXISTS sp_insert_csg;
+
 DELIMITER $
-CREATE PROCEDURE sp_insert_csg_doc_flow (tbl_name VARCHAR(30), name VARCHAR(255), city_id BIGINT,
+CREATE PROCEDURE sp_insert_csg (tbl_name VARCHAR(30), name VARCHAR(255), city_id BIGINT,
 address VARCHAR(255), email VARCHAR(50), phone BIGINT, cio VARCHAR(100), cto VARCHAR(100))
 BEGIN
 	SET @sql = CONCAT('INSERT INTO ', tbl_name, ' VALUES', '(', 'NULL', ', ', name, ', ', city_id, ', ', address, ', ', 
 	email, ', ', phone, ', ' , cio, ', ' , cto, ');');
 	PREPARE stmt FROM @sql;
 	EXECUTE stmt;
+
 END $
+
 DELIMITER ;
 
-SHOW PROCEDURE STATUS LIKE '%doc_flow';
-
-CALL sp_insert_csg_doc_flow ('customers', '"МЭС Юга"', '5', '"пер. Дарницкий 2"', '"org@umes.kmv.ru"', '88793343611', 
+-- to check
+CALL sp_insert_csg ('customers', '"МЭС Юга"', '5', '"пер. Дарницкий 2"', '"org@umes.kmv.ru"', '88793343611', 
 '"Петров А.А."', '"Иванов Б.Б."');
-CALL sp_insert_csg_doc_flow ('subcontractors', '"НТЦ ФСК ЕЭС"', '1', '"Каширское ш., д.22к3"', '"info@ntc-power.ru"', '74957271909', 
+CALL sp_insert_csg ('subcontractors', '"НТЦ ФСК ЕЭС"', '1', '"Каширское ш., д.22к3"', '"info@ntc-power.ru"', '74957271909', 
 '"Стулов А.А."', '"Столов Б.Б."');
 
--- 3. to add some new project
-DROP PROCEDURE IF EXISTS sp_add_project_doc_flow;
+-- 3. procedure to add some new project
+DROP PROCEDURE IF EXISTS sp_add_project;
 
 DELIMITER $
-CREATE PROCEDURE sp_add_project_doc_flow (title VARCHAR(500), contract VARCHAR(30), cust VARCHAR(100), gen VARCHAR(100), sub VARCHAR(100),
+CREATE PROCEDURE sp_add_project (title VARCHAR(500), contract VARCHAR(30), cust VARCHAR(100), gen VARCHAR(100), sub VARCHAR(100),
 `start` DATE, expertise VARCHAR(100), OUT tran_result VARCHAR(200))
 BEGIN
 	DECLARE `_rollback` BOOL DEFAULT 0;
-	DECLARE code VARCHAR(100);
+	DECLARE error_code VARCHAR(100);
 	DECLARE error_string VARCHAR(100);
 	DECLARE cust_id BIGINT;
 	DECLARE gen_id BIGINT;
@@ -65,8 +70,8 @@ BEGIN
 	BEGIN
 		SET `_rollback` = 1;
 		GET stacked DIAGNOSTICS CONDITION 1
-			code = RETURNED_SQLSTATE, error_string = MESSAGE_TEXT;
-		SET tran_result := CONCAT('Error occured. Code: ', code, '. Text: ', error_string);
+			error_code = RETURNED_SQLSTATE, error_string = MESSAGE_TEXT;
+		SET tran_result := CONCAT('Error occured. Code: ', error_code, '. Text: ', error_string);
 	END;
 
 	SET cust_id = (SELECT id FROM customers WHERE name = cust);
@@ -86,38 +91,74 @@ BEGIN
 
 END $
 
-DELIMITER ;
-
 -- insert some valid data
-CALL sp_add_project_doc_flow ('ПС 220 кВ Озерная с заходами ВЛ 220 кВ', 'ПИР-175', 'ПАО ФСК ЕЭС МЭС Сибири', 'ООО АРСЕНАЛ ПЛЮС',
+CALL sp_add_project ('ПС 220 кВ Озерная с заходами ВЛ 220 кВ', 'ПИР-175', 'ПАО ФСК ЕЭС МЭС Сибири', 'ООО АРСЕНАЛ ПЛЮС',
 'ЗАО Энергопроект', '2020-08-06', 'ГАУ Госэкспертиза Кузбасса', @tran_result);
 
 SELECT @tran_result;
 
 -- try to insert some invalid data
-CALL sp_add_project_doc_flow ('ПС 220 кВ Озерная с заходами ВЛ 220 кВ', 'ПИР-175', 'ПАО ФСК ЕЭС МЭС Сибири', 'ООО АРСЕНАЛ ПЛЮС',
+CALL sp_add_project ('ПС 220 кВ Озерная с заходами ВЛ 220 кВ', 'ПИР-175', 'ПАО ФСК ЕЭС МЭС Сибири', 'ООО АРСЕНАЛ ПЛЮС',
 'ЗАО Энергопроект', '2020-08-06', 'ГАУ Госэкспертиза Китая', @tran_result);
 
 SELECT @tran_result;
 
-SHOW PROCEDURE STATUS LIKE '%doc_flow';
+-- 4. procedure to add some new documentation
+DROP PROCEDURE IF EXISTS sp_add_docum;
 
--- 4. to add some new documentation
-DROP PROCEDURE IF EXISTS sp_add_docum_doc_flow;
 DELIMITER $
-CREATE PROCEDURE sp_add_docum_doc_flow (code VARCHAR(30), name VARCHAR(255), p_id BIGINT, s_id BIGINT, `start` DATE, 
-cur_id BIGINT)
+CREATE PROCEDURE sp_add_docum (code VARCHAR(30), name VARCHAR(255), p_id BIGINT, stage VARCHAR(10), 
+`start` DATE, cur VARCHAR(30), OUT tran_result VARCHAR(200))
 BEGIN
-	INSERT INTO documentation (code, name, project_id, stage_id, started_at, curator_id) VALUES
-	(code, name, p_id, s_id, `start`, cur_id);
+	DECLARE `_rollback` BOOL DEFAULT 0;
+	DECLARE error_code VARCHAR(100);
+	DECLARE error_string VARCHAR(100);
+	DECLARE stg_id BIGINT;
+	DECLARE cur_id BIGINT;
+
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+	BEGIN
+		SET `_rollback` = 1;
+		GET stacked DIAGNOSTICS CONDITION 1
+			error_code = RETURNED_SQLSTATE, error_string = MESSAGE_TEXT;
+		SET tran_result := CONCAT('Error occured. Code: ', error_code, '. Text: ', error_string);
+	END;
+
+	SELECT id INTO stg_id FROM stages WHERE stages.name = stage;
+	SELECT id INTO cur_id  FROM employees WHERE fullname = cur;
+	
+	START TRANSACTION;
+		INSERT INTO documentation (code, name, project_id, stage_id, started_at, curator_id) VALUES
+		(code, name, p_id, stg_id, `start`, cur_id);
+		IF `_rollback` THEN
+			ROLLBACK;
+		ELSE
+			SET tran_result = 'Successfully';
+			COMMIT;
+		END IF;
+
 END $
+
 DELIMITER ;
--- 'ПС 220 кВ Озерная с заходами ВЛ 220 кВ' project_id = 16 after calling previous add_project_doc_flow procedure
-CALL sp_add_docum_doc_flow 
-('П0750-ИОС4.1', 'Автоматизированная система управления технологическими процессами', 16, 1, '2020-08-01', 2);
-CALL sp_add_docum_doc_flow 
-('П0750-ИОС4.2', 'Релейная защита и противоаварийная автоматика', 16, 1, '2020-08-01', 2);
-CALL sp_add_docum_doc_flow 
-('П0750-ИОС4.3', 'Сети связи', 16, 1, '2020-08-01', 2);
-CALL sp_add_docum_doc_flow
-('П0750-ППО', 'Предпроектное обследование', 16, 4, '2020-07-20', 2);
+
+/* add some new documentation for project 'ПС 220 кВ Озерная с заходами ВЛ 220 кВ'
+ which has project_id = 16 after calling previous sp_add_project procedure
+ */
+CALL sp_add_docum 
+('П0750-ИОС4.1', 'Автоматизированная система управления технологическими процессами', 16, 'ПД', '2020-08-01', 'Соколов Владимир', @tran_result);
+CALL sp_add_docum 
+('П0750-ИОС4.2', 'Релейная защита и противоаварийная автоматика', 16, 'ПД', '2020-08-01', 'Соколов Владимир', @tran_result);
+CALL sp_add_docum 
+('П0750-ИОС4.3', 'Сети связи', 16, 'ПД', '2020-08-01', 'Соколов Владимир', @tran_result);
+CALL sp_add_docum
+('П0750-ППО', 'Предпроектное обследование', 16, 'ППО', '2020-07-20', 'Соколов Владимир', @tran_result);
+
+SELECT @tran_result;
+
+-- try to insert some invalid data
+CALL sp_add_docum
+('П0750-КД', 'Предпроектное обследование', 16, 'ППО', '2020-07-20', 'Неизвестная личность', @tran_result);
+
+SELECT @tran_result;
+
+
